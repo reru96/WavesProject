ef2c0d6d05bf43c47b7e73a89e23f1fc71612cc1
reru96 <renatorumieri96@outlook.com>
2025-11-12 17:16:05 +0100

    aggiunta CineMachine

--- Assets/Scripts/Camera/CameraManagere.cs
+++ Assets/Scripts/Camera/CameraManagere.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class CameraManagere : MonoBehaviour
@@ -5,18 +6,20 @@ public class CameraManagere : MonoBehaviour
     [SerializeField] private Transform _cameraPivot; 
     [SerializeField] private Vector3 _offset = new Vector3(0f, 0f, -10f);
     [SerializeField] private float _smoothSpeed = 1f;
+    public CinemachineCamera cineMachine;
 
     private void Start()
     {
         GameObject orientation = GameObject.FindGameObjectWithTag("Orientation");
-        _cameraPivot = orientation.transform;
+        cineMachine.Follow = orientation.transform;
     }
 
     void LateUpdate()
-    {
-        if (_cameraPivot == null) return;
-
-        Vector3 targetPos = _cameraPivot.position + _offset;
-        transform.position = Vector3.Lerp(transform.position, targetPos, _smoothSpeed * Time.deltaTime);
+    { 
+        
+        //if (_cameraPivot == null) return;
+       
+        //Vector3 targetPos = _cameraPivot.position + _offset;
+        //transform.position = Vector3.Lerp(transform.position, targetPos, _smoothSpeed * Time.deltaTime);
     }
 }
